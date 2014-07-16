9ef1f0871001697d072ec4480a3111960c1e6275
lensing <339220156@qq.com>
2014-07-17 00:19:29 +0800

    add product to webaa1

--- webapp1/db/seeds.rb
+++ webapp1/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+100.times do |i|
+Product.create(:title => "Programming Ruby #{i}",
+	       :description => 'Ruby is the faster growing and most exciting dynamic language out there',
+	       :image_url => '/images/ruby.jpg',
+               :price => 49.50)
+end	
