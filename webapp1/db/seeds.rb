576ff88625bec2969cdf427cdc9324b16e76b27b
lensing <339220156@qq.com>
2014-07-17 22:53:47 +0800

    2014-07-17 main layout

--- webapp1/db/seeds.rb
+++ webapp1/db/seeds.rb
@@ -3,6 +3,6 @@ Product.delete_all
 100.times do |i|
 Product.create(:title => "Programming Ruby #{i}",
 	       :description => 'Ruby is the faster growing and most exciting dynamic language out there',
-	       :image_url => '/images/ruby.jpg',
-               :price => 49.50)
+	       :image_url => 'ruby.jpg',
+               :price => 49.58)
 end	
